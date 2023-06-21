150fbc01991851c3bc6cc35d77acacb8ccb331a7
Eduardo <eduaraujp760@gmail.com>
2023-06-21 17:23:09 -0400

    Atividade: Debugando com console.log()

---
+++ debug.js
@@ -0,0 +1,68 @@
+/* //Exercicio 1
+function verificaNumero(num) {
+  let str = '';
+  
+  if (num % 2 == 0) {
+    str = 'par';
+  } else {
+    str = 'impar';
+  }
+
+  str += ', ';
+
+  if (num > 0) {
+    str += 'positivo';
+  } else {
+    str += 'negativo';
+  }
+
+  str += ' e ';
+
+  if (num === parseInt(num)) {
+    str += 'não possui casas decimais';
+  } else {
+    str += 'possui casas decimais';
+  }
+
+  str = 'O numero ' + num + ' é ' + str;
+
+  return str;
+}
+
+console.log(verificaNumero(-12.02)); */
+
+//Exercicio 2
+let valor = 18;
+let result = valor / 2;
+console.log(result);
+
+result = result / 3;
+console.log(result);
+
+let isPair = result == 3 ;
+console.log(isPair);
+
+if (isPair) {
+  result = result * 6;
+} else {
+  result = result * 12;
+}
+
+console.log(result);
+
+//Exercicio 3
+function algarismosEmUmNumero(n){
+    let str = `${n}`;
+    let contagem = str.length;
+    console.log(contagem);
+  
+    if (n != parseInt(n)) {
+      console.log("Entrou aqui");
+      contagem = contagem - 1;
+    }
+  
+    console.log(contagem);
+  
+    return contagem;
+  }
+  algarismosEmUmNumero(3.141517);
